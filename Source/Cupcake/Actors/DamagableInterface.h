cb4acfc1045b2342ac00e22693e6c337519f2e0f
ZrenKix <50847546+ZrenKix@users.noreply.github.com>
2024-04-24 11:17:46 +0200

    Fixed errors

--- Source/Cupcake/Actors/DamagableInterface.h
+++ Source/Cupcake/Actors/DamagableInterface.h
@@ -7,21 +7,16 @@
 #include "DamagableInterface.generated.h"
 
 class UHealthComponent;
-// This class does not need to be modified.
+
 UINTERFACE(MinimalAPI)
 class UHealth : public UInterface
 {
 	GENERATED_BODY()
 };
 
-/**
- * 
- */
 class CUPCAKE_API IHealth
 {
 	GENERATED_BODY()
-
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	UHealthComponent* HealthComponent;
 	

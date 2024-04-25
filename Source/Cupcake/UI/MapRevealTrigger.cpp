2c8aa3381680e3e3c8e3d8ad9a2063865c1cad48
linpack12 <55255203+linpack12@users.noreply.github.com>
2024-04-25 11:59:52 +0200

    deltid

--- Source/Cupcake/UI/MapRevealTrigger.cpp
+++ Source/Cupcake/UI/MapRevealTrigger.cpp
@@ -40,25 +40,7 @@ void AMapRevealTrigger::Tick(float DeltaTime)
 void AMapRevealTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	// Check if the other actor is valid, not this trigger, and the other component is also valid
-	if (OtherActor && OtherActor != this && OtherComp)
-	{
-		// Check if the overlapping actor is the player
-		if (OtherActor->ActorHasTag(FName("Player")))
-		{
-			// Retrieve the user widget subsystem from the game instance
-			if (UUserWidgetSubsystem* WidgetSubsystem = UGameplayStatics::GetGameInstance(this)->GetSubsystem<UUserWidgetSubsystem>())
-			{
-				// Retrieve the specific map widget by its name
-				if (UUserWidget* MapWidget = Cast<UUserWidget>(WidgetSubsystem->GetWidget(FName("UI_MapWidget"))))
-				{
-					// Call the function to reveal the map part, assuming MapPartTag is a FName member variable of this class
-					FString Command = FString::Printf(TEXT("RevealMapPart %s"), *MapPartTag.ToString());
-					MapWidget->CallFunctionByNameWithArguments(*Command, nullptr, nullptr, true);
-				}
-			}
-		}
-	}
+
 }
 	
 

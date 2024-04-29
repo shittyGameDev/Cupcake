7d5e62be2341577b6cf47b19b552d5e55f304358
RexRegum <56251425+RexRegum@users.noreply.github.com>
2024-04-29 15:21:28 +0200

    Update from main

---
+++ Source/Cupcake/Actors/AttributeComponent.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AttributeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CUPCAKE_API UAttributeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UAttributeComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+<<<<<<< HEAD:Source/Cupcake/Actors/HealthComponent.h
+	bool RegenerateHealth(float HealthAmount);
+	
+	void DoDamage(float DamageAmount);
+};
+=======
+private:
+	// Current Health
+	UPROPERTY(EditAnywhere, Category="Actor Attributes")
+	float Health = 100.f;
+	
+	UPROPERTY(EditAnywhere, Category="Actor Attributes")
+	float MaxHealth = 100.f;
+
+public:
+	void ReceiveDamage(float Damage);
+};
+>>>>>>> main:Source/Cupcake/Actors/AttributeComponent.h
---
+++ Source/Cupcake/TheMapObject.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "TheMapObject.generated.h"
+
+/**
+ * Klass som representerar objektet som aktiverar kartwidgeten när spelaren närmar sig och interagerar.
+ */
+UCLASS()
+class CUPCAKE_API ATheMapObject : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATheMapObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Sphere component to handle overlap events
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* CollisionComponent;
+
+	// Static mesh to represent the object visually
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* Mesh;
+
+	// Widget to show when the map object is interacted with
+	UPROPERTY(Transient)
+	UUserWidget* MapWidget;
+
+	// Widget class to create from
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> MapWidgetClass;
+
+public: 
+	// Function to handle the beginning of an overlap
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                    int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// Function to toggle the visibility of the map widget
+	void ToggleMapVisibility();
+};

748d50db7c2fec3ab41782df15602b7927e7202e
ZrenKix <50847546+ZrenKix@users.noreply.github.com>
2024-05-02 13:13:28 +0200

    Stuff

--- Source/Cupcake/AI/GangAIController.cpp
+++ Source/Cupcake/AI/GangAIController.cpp
@@ -1,5 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AI/GangAIController.h"
-
+#include "GangAIController.h"
--- Source/Cupcake/World/DayCycleManager.cpp
+++ Source/Cupcake/World/DayCycleManager.cpp
@@ -11,9 +11,8 @@
 // Sets default values
 ADayCycleManager::ADayCycleManager()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
 }
 
 // Called when the game starts or when spawned

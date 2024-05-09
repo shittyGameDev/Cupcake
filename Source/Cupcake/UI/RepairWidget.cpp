0d76ef42f39502d95d0fe2ae8cffdfe45d10b874
shittyGameDev <129396037+shittyGameDev@users.noreply.github.com>
2024-05-09 10:19:24 +0200

    repair

---
+++ Source/Cupcake/UI/RepairWidget.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RepairWidget.h"
+
+
+void URepairWidget::IncreaseWoodQuantity(int NewWoodQuantity)
+{
+	
+}
+
+void URepairWidget::IncreaseStoneQuantity(int NewWoodQuantity)
+{
+}
+
+void URepairWidget::IncreaseIronQuantity(int NewWoodQuantity)
+{
+}

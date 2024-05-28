2de455e6239e321d71187b2e1dd8a192111ff7fa
ZrenKix <50847546+ZrenKix@users.noreply.github.com>
2024-05-28 13:31:02 +0200

    Extended Save and added zinded to DCM
    Im gonna puke...

--- Source/Cupcake/CupcakeGameMode.cpp
+++ Source/Cupcake/CupcakeGameMode.cpp
@@ -2,38 +2,123 @@
 
 #include "CupcakeGameMode.h"
 
+#include "Items/BaseItem.h"
+#include "Kismet/GameplayStatics.h"
+#include "PlayerSystem/NewInventoryComponent.h"
+#include "PlayerSystem/SavePlayerProgress.h"
+#include "World/WorldSystem/DayCycleManager.h"
+
 void ACupcakeGameMode::BeginPlay()
 {
-	 Super::BeginPlay();
+	Super::BeginPlay();
+
+	// Set references
+	PlayerCharacter = Cast<ACupcakeCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	PlayerInventory = PlayerCharacter->GetInventory();
 }
 
 
-void ACupcakeGameMode::AddTriggerBox(bool bBoolValue, const FName& BoxName)
+//SAVE SYSTEM
+//------------------------------------------------------------------
+
+FItemSaveData ConvertToSaveData(UBaseItem* Item)
 {
-	// Add the bool value and trigger box name to the arrays
-	TriggerBoxBoolArray.Add(bBoolValue);
-	TriggerBoxNames.Add(BoxName);
+	FItemSaveData SaveData;
+	SaveData.ID = Item->ID;
+	SaveData.Quantity = Item->Quantity;
+	SaveData.ItemType = Item->ItemType;
+	SaveData.TextData = Item->TextData;
+	SaveData.NumericData = Item->NumericData;
+	SaveData.AssetData = Item->AssetData;
+	return SaveData;
 }
 
-bool ACupcakeGameMode::GetTriggerBoxBool(const FName& BoxName) const
+UBaseItem* ConvertToBaseItem(UObject* Outer, const FItemSaveData& SaveData)
 {
-	// Find the index of the trigger box name
-	int32 Index = TriggerBoxNames.IndexOfByKey(BoxName);
-	if (Index != INDEX_NONE)
-	{
-		// Return the bool value corresponding to the trigger box name
-		return TriggerBoxBoolArray[Index];
-	}
-	return false;
+	UBaseItem* Item = NewObject<UBaseItem>(Outer);
+	Item->ID = SaveData.ID;
+	Item->Quantity = SaveData.Quantity;
+	Item->ItemType = SaveData.ItemType;
+	Item->TextData = SaveData.TextData;
+	Item->NumericData = SaveData.NumericData;
+	Item->AssetData = SaveData.AssetData;
+	return Item;
 }
 
-void ACupcakeGameMode::LogTriggerBoxes() const
+void ACupcakeGameMode::SaveGame()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Logging Trigger Boxes:"));
+	// Set references
+	PlayerCharacter = Cast<ACupcakeCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	PlayerInventory = PlayerCharacter->GetInventory();
 	
-	for (int32 Index = 0; Index < TriggerBoxNames.Num(); ++Index)
+	USavePlayerProgress* SaveGameInstance = Cast<USavePlayerProgress>(UGameplayStatics::CreateSaveGameObject(USavePlayerProgress::StaticClass()));
+	
+	//Get Inventory and save it (Victor)
+	//Can be called in BP
+	if (PlayerInventory)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Trigger Box Name: %s, Bool Value: %s"), *TriggerBoxNames[Index].ToString(), TriggerBoxBoolArray[Index] ? TEXT("true") : TEXT("false"));
+		for (UBaseItem* Item : PlayerInventory->GetInventoryContents())
+		{
+			SaveGameInstance->InventoryItems.Add(ConvertToSaveData(Item));
+		}
 	}
+
+	// Get current Day (William)
+	ADayCycleManager* DayCycleManager = Cast<ADayCycleManager>(UGameplayStatics::GetActorOfClass(GetWorld(), ADayCycleManager::StaticClass()));
+	if (DayCycleManager)
+	{
+		SaveGameInstance->DayNumber = DayCycleManager->GetCurrentDayNumber();
+		UE_LOG(LogTemp, Warning, TEXT("Day cycle number: %d"), DayCycleManager->GetCurrentDayNumber());
+	}
+	
+	UGameplayStatics::SaveGameToSlot(SaveGameInstance, TEXT("PlayerSaveSlot"), 0);
+	UE_LOG(LogTemp, Warning, TEXT("Saving Game"));
 }
 
+void ACupcakeGameMode::LoadGame()
+{
+	// Set references
+	PlayerCharacter = Cast<ACupcakeCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	PlayerInventory = PlayerCharacter->GetInventory();
+    	
+	USavePlayerProgress* LoadGameInstance = Cast<USavePlayerProgress>(UGameplayStatics::LoadGameFromSlot(TEXT("PlayerSaveSlot"), 0));
+
+	// Set everything we saved in the PlayerSaveSlot. Can be called in BP
+	if (LoadGameInstance)
+	{
+		// Load inventory
+		if (PlayerInventory)
+		{
+			// Clear the inventory first
+			PlayerInventory->ClearInventory();
+
+			// Load each item from the save data
+			for (const FItemSaveData& SaveData : LoadGameInstance->InventoryItems)
+			{
+				UBaseItem* NewItem = ConvertToBaseItem(this, SaveData);
+				PlayerInventory->HandleLoadItem(NewItem);
+			}
+		}
+
+		// Load day
+		ADayCycleManager* DayCycleManager = Cast<ADayCycleManager>(UGameplayStatics::GetActorOfClass(GetWorld(), ADayCycleManager::StaticClass()));
+		if (DayCycleManager)
+		{
+			for (int i = 0; i < LoadGameInstance->DayNumber - 1; i++)
+			{
+				if (i != LoadGameInstance->DayNumber - 1)
+				{
+					DayCycleManager->ShiftDay();
+				}
+				else
+				{
+					DayCycleManager->ShiftDay(1001);
+				}
+				
+			}
+			UE_LOG(LogTemp, Warning, TEXT("Target Day cycle: %d"), LoadGameInstance->DayNumber);
+			UE_LOG(LogTemp, Warning, TEXT("Curent day: %d"), DayCycleManager->GetCurrentDayNumber());
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Loaded Save"));
+}
